735680f454f3980135faf8b581194762897033b7
aruzhan-karamelka <aruzhan.myrza.06@gmail.com>
2026-05-18 11:42:11 +0500

    8 tutorial

--- Roll-a-Ball/Assets/Scripts/PlayerController.cs
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -53,4 +53,13 @@ public class PlayerController : MonoBehaviour
             SetCountText();
         }
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+        }
+    }
 }
